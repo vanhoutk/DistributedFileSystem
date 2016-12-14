9ce3ef2f61c331bf87a9b1598958a278973874d3
Vanhoutk <vanhoutk@tcd.ie>
2016-12-14 17:02:00 +0000

    Just commiting progress so far, nothing working yet

---
+++ DFS-Client/src/Lib.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+module Lib
+    ( startApp
+    ) where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = do
+	--manager <- newManager defaultManagerSettings
+	--res <- runClientM queries (ClientEnv manager (BaseUrl Http "localhost" 8080 ""))
+	--case res of
+	--	Left err -> putStrLn $ "Error: " ++ show err
+	--	Right (user) -> do
+	--		print user
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
---
+++ fileserver/src/Lib.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+
+module Lib
+    ( startApp
+    ) where
+
+import 						Data.Aeson
+import						Data.Aeson.TH
+import qualified	Data.List                    as DL
+import           	Data.Maybe                   (catMaybes)
+import           	Database.MongoDB
+import 						Network.Wai
+import 						Network.Wai.Handler.Warp
+import 						Servant
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = uploadFile
+	:<|> downloadFile
+
+	where
+
+		uploadFile :: Message -> Handler Bool
+		uploadFile msg@(Message key _) = liftIO $ do
+			putStrLn $ "Storing file under key " ++ key ++ "."
+			withMongoDbConnection $ upsert (select ["name" =: key] "MESSAGE_RECORD") $ toBSON msg
+			return True
+
+		downloadFile :: Maybe String -> Handler [Message]
+		downloadFile (Just key) = liftIO $ do
+			putStrLn $ "Searching for value for key: " ++ key
+			withMongoDbConnection $ do
+				docs <- find (select ["name" =: key] "MESSAGE_RECORD") >>= drainCursor
+				return $ catMaybes $ DL.map (\ b -> fromBSON b :: Maybe Message) docs
+
+		downloadFile Nothing = liftIO $ do
+      putStrLn $ "No key for searching."
+      return $ ([] :: [Message])
+
+
+
+-- | Mongodb helpers...
+
+-- | helper to open connection to mongo database and run action
+-- generally run as follows:
+--        withMongoDbConnection $ do ...
+--
+withMongoDbConnection :: Action IO a -> IO a
+withMongoDbConnection act  = do
+  ip <- mongoDbIp
+  port <- mongoDbPort
+  database <- mongoDbDatabase
+  pipe <- connect (host ip)
+  ret <- runResourceT $ liftIO $ access pipe master (pack database) act
+  close pipe
+  return ret
+
+-- | helper method to ensure we force extraction of all results
+-- note how it is defined recursively - meaning that draincursor' calls itself.
+-- the purpose is to iterate through all documents returned if the connection is
+-- returning the documents in batch mode, meaning in batches of retruned results with more
+-- to come on each call. The function recurses until there are no results left, building an
+-- array of returned [Document]
+drainCursor :: Cursor -> Action IO [Document]
+drainCursor cur = drainCursor' cur []
+  where
+    drainCursor' cur res  = do
+      batch <- nextBatch cur
+      if null batch
+        then return res
+        else drainCursor' cur (res ++ batch)
+
+-- | Environment variable functions, that return the environment variable if set, or
+-- default values if not set.
+
+-- | The IP address of the mongoDB database that devnostics-rest uses to store and access data
+mongoDbIp :: IO String
+mongoDbIp = defEnv "MONGODB_IP" id "database" True
+
+-- | The port number of the mongoDB database that devnostics-rest uses to store and access data
+mongoDbPort :: IO Integer
+mongoDbPort = defEnv "MONGODB_PORT" read 27017 False -- 27017 is the default mongodb port
+
+-- | The name of the mongoDB database that devnostics-rest uses to store and access data
+mongoDbDatabase :: IO String
+mongoDbDatabase = defEnv "MONGODB_DATABASE" id "USEHASKELLDB" True
+
+-- | Helper function to simplify the setting of environment variables
+-- function that looks up environment variable and returns the result of running funtion fn over it
+-- or if the environment variable does not exist, returns the value def. The function will optionally log a
+-- warning based on Boolean tag
+defEnv :: Show a
+              => String        -- Environment Variable name
+              -> (String -> a)  -- function to process variable string (set as 'id' if not needed)
+              -> a             -- default value to use if environment variable is not set
+              -> Bool          -- True if we should warn if environment variable is not set
+              -> IO a
+defEnv env fn def doWarn = lookupEnv env >>= \ e -> case e of
+      Just s  -> return $ fn s
+      Nothing -> do
+        when doWarn (doLog warningM $ "Environment variable: " ++ env ++
+                                      " is not set. Defaulting to " ++ (show def))
+        return def
